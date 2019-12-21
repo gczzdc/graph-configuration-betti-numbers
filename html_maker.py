9fbb6416edabbeac257e41e0157441587d394217
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-21 10:42:24 -0800

    first (unusable) draft breaking html off separately

---
+++ html_maker.py
@@ -0,0 +1,220 @@
+todolist=[\
+'need better format for graph name',\
+'need to add check for existence of image',\
+'need graph data class',\
+'import constants dynamically',\
+'use beautifulsoup for html generation',\
+'rewrite format_macaulay',\
+]
+
+graphics_format = 'svg'
+
+#
+#
+# TEXT FOR PRESENTATION
+#
+#
+intro_file_base='intro'
+outro_file_base='outro'
+
+
+data_section_title="Data for small graphs"
+
+
+
+graph_html_header_builder=[]
+graph_html_header_builder.append((
+	'</section>\n'
+	'<section>\n'
+	"<h2>"
+	))
+graph_html_header_builder.append(data_section_title)
+graph_html_header_builder.append((
+	'</h2>\n'
+	))
+
+graph_html_header=''.join(graph_html_header_builder)
+
+
+
+def format_macaulay_html(pair):
+	#data is a tuple with first element a denom_power string
+	#second element a macaulay poly numerator
+	answer=[]
+	poly = pair[0]
+	denom_power = pair[1]
+	rational_string=''
+	is_monomial=0
+	for coefficient in poly:
+		if coefficient:
+			is_monomial+=1
+	if is_monomial!=1 and denom_power>0:
+		rational_string+='('
+	rational_string+=format_poly_to_str(poly)
+	if is_monomial!=1 and denom_power>0:
+		rational_string+=')'
+	if denom_power>0:
+		rational_string+= '/(1-t)'
+	if denom_power>1:
+		rational_string+='<sup>'+str(denom_power)+'</sup>'
+	answer.append(rational_string)
+	c_poly=hilb_series_to_coefficient_poly(poly,denom_power)[::-1]
+	if denom_power>2:
+		c_poly_str='('+format_poly_to_str(c_poly,format='html',var='k')+')'
+		c_poly_str +='/'+str(denom_power-1)+'!'
+	else:
+		c_poly_str=format_poly_to_str(c_poly,format='html',var='k')
+	valid='k &gt; ' +str(len(poly)-denom_power-1)
+	answer.append(c_poly_str)
+	answer.append(valid)
+	return(answer)
+
+def assemble_table_for_html(graph,data):
+		#data is an object so that it's extendible
+	#data should have Betti numbers
+	output_builder=[]
+	output_builder.append('<div class="row">\n')
+	# output_builder.append('<div class="colleft" style="width:50%;float:left;text-align:right">\n')
+	output_builder.append('<div class="colleft">\n')
+	output_builder.append('<p><img src="')
+	output_builder.append(graph)
+	output_builder.append('.')
+	output_builder.append(graphics_format)
+	output_builder.append('" alt="picture of the graph ')
+	output_builder.append(graph)
+	output_builder.append('" ')
+	output_builder.append('style="margin-right:20px"')#;margin-bottom:20px;margin-top:10px"')
+	output_builder.append('></p>\n')
+	output_builder.append('</div>\n')
+	# output_builder.append('<div class="colright" style="width:50%;float:left;text-align:left">\n')
+	output_builder.append('<div class="colright">\n')
+	output_builder.append('<table class="matrix" ')
+	output_builder.append('style="display:inline-block;margin-left:20px">\n')#;margin-bottom:20px;margin-top:10px">\n')
+	for i in range(len(data.adjacency)):
+		output_builder.append('<tr>\n')
+		for j in range(len(data.adjacency[0])):
+			output_builder.append('<td class="matrixcell">\n')
+			output_builder.append(str(data.adjacency[i][j]))
+			output_builder.append('</td>\n')
+		output_builder.append('</tr>\n')
+	output_builder.append('</table>\n')
+	output_builder.append('</div>\n')
+	output_builder.append('</div>\n')
+	# output_builder.append('<p class="centered">\n')
+	# output_builder.append('<img src="')
+	# output_builder.append(graph)
+	# output_builder.append('.')
+	# output_builder.append(graphics_format)
+	# output_builder.append('" alt="picture of the graph ')
+	# output_builder.append(graph)
+	# output_builder.append('">\n')
+	# output_builder.append('<table class="matrix">\n')
+	# for i in range(len(data.adjacency)):
+	# 	output_builder.append('<tr>\n')
+	# 	for j in range(len(data.adjacency[0])):
+	# 		output_builder.append('<td class="matrixcell">\n')
+	# 		output_builder.append(str(data.adjacency[i][j]))
+	# 		output_builder.append('</td>\n')
+	# 	output_builder.append('</tr>\n')
+	# output_builder.append('</table>\n')
+	# output_builder.append('</p>\n')
+	output_builder.append('<div style="width:100%;clear:both"><p class="centered">\n')
+	if data.note:
+		output_builder.append('Note: ')
+		output_builder.append(data.note)
+		output_builder.append('\n<br>\n')
+	if data.Betti_numbers:
+		output_builder.append('Betti numbers &beta;<sub>i</sub>(B<sub>k</sub>(&Gamma;)):')
+		output_builder.append('\n</p>\n<p class="centered">')
+		row_length = max([len(row) for row in data.Betti_numbers.values()])
+		cap = min(row_length,Betti_row_max_length)
+		output_builder.append('<table class="centered">\n')
+		output_builder.append('<tr>\n')
+		output_builder.append('<th>i&#92;k</th>\n')
+		for col_number in range(cap):
+			output_builder.append('<th>')
+			output_builder.append(str(col_number))
+			output_builder.append('</th>\n')
+		output_builder.append('<th>')
+		output_builder.append('Poincar&eacute; series')
+		output_builder.append('</th>\n')
+		output_builder.append('<th>')
+		output_builder.append('stable polynomial value')
+		output_builder.append('</th>\n')
+		output_builder.append('</tr>\n')
+		for row_number in range(data.homological_degree+1):
+			output_builder.append('<tr>\n<th>')
+			output_builder.append(str(row_number))
+			output_builder.append('</th>\n')
+			for col_number in range(min(len(data.Betti_numbers[row_number]),cap)):
+				output_builder.append('<td>')
+				if (row_number,col_number) in data.Betti_number_is_unstable:
+					output_builder.append('<span style="font-weight:900">')
+				output_builder.append(str(data.Betti_numbers[row_number][col_number]))
+				if (row_number,col_number) in data.Betti_number_is_unstable:
+					output_builder.append('</span>')
+				output_builder.append('</td>\n')
+			for col_number in range(cap-min(len(data.Betti_numbers[row_number]),cap)):
+				output_builder.append('<td></td>\n')
+			output_builder.append('<td>')
+			html_polys = format_macaulay_html(data.polys[row_number])
+			output_builder.append(html_polys[0])
+			output_builder.append('</td>\n')
+			output_builder.append('<td>')
+			output_builder.append(html_polys[1])
+			# output_builder.append(data.htmltriples[row_number][0])
+			# output_builder.append(data.htmltriples[row_number][1])
+			output_builder.append('</td>\n')
+			output_builder.append('</tr>\n')
+		output_builder.append('</table>\n')
+	else:
+		output_builder.append('No Betti number data available')
+	output_builder.append('</p></div>\n')
+	output_builder.append('<hr>\n')
+	return (''.join(output_builder))
+
+def graph_html_section_maker(n):
+	output_builder=	['<h3><a id=',]
+	output_builder.append(str(n))
+	output_builder.append("_vertices></a>\n Data for graphs with ")
+	output_builder.append(str(n))
+	if n !=1:
+		output_builder.append(' essential vertices\n</h3>\n<hr>\n')
+	else:
+		output_builder.append(' essential vertex\n</h3>\n<hr>\n')	
+	return (''.join(output_builder))
+
+def assemble_html(graph_list,data_dic):
+	# graph_list is a list or dictionary 
+	# with integer keys n 
+	# and values iterators of 
+	# graphs with n vertices
+	output_builder=[]
+	with open(intro_file_base + '.html','r') as f:
+		output_builder.append(f.read())
+	output_builder.append(graph_html_header)
+	output_builder.append('<div><ul>\n')
+	for j in range(len(graph_list)):
+		output_builder.append('<li><a href="#')
+		output_builder.append(str(j))
+		output_builder.append('_vertices">Graphs with ')
+		output_builder.append(str(j))
+		output_builder.append(' vertices</a></li>\n')
+	output_builder.append('</ul></div>\n')
+	for j in range(len(graph_list)):
+		if graph_list[j]:
+			output_builder.append(graph_html_section_maker(j))
+			for graph in graph_list[j]:
+				if graph in data_dic:
+					output_builder.append(assemble_table_for_html(graph,data_dic[graph]))
+				else: #make an empty information thing
+					output_builder.append(assemble_table_for_html(graph,data_class()))
+	# output_builder.append(graph_html_footer)
+	with open(outro_file_base+'.html','r') as f:
+		output_builder.append(f.read())
+	return (''.join(output_builder))
+
+def write_html(graph_list,data_dic,file_name):
+	with open(file_name, 'w') as f:
+		f.write(assemble_html(graph_list,data_dic))
+
