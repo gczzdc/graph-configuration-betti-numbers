3b8d7d9283178a59c7e927ff032f6b55481f8e5e
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 14:13:11 -0800

    added flags, renamed variables to get rid of underscores

--- graph_class.py
+++ graph_class.py
@@ -1,47 +1,56 @@
 class Graph():
 	def __init__(self):
-		self.vertices_=None
-		self.edges_=None
-		self.half_edges_=None
-		self.adjacency_=None
-		self.sparse6_=None
+		self.vertices=None
+		self.edges=None
+		self.half_edges=None
+		self.adjacency=None
+		self.sparse6=None
+
+		self.has_sparse6 = False
+		self.has_VE = False
+		self.has_adjacency = False
+		self.has_HVi = False
 
 		self.name=None
 		self.image_file=None
 		self.note=None
 
-		self.essential_vertices_=None
+		self.essential_vertices=None
 
 		self.homological_degree=-1
 		self.Betti_numbers={}
 		self.Betti_number_is_unstable=set()
 		self.graph.poincare_num_poly=None,
 		self.graph.poincare_denom_power=None,
-		self.graph.stable_poly_normalized=None
-	
+		self.graph.stable_poly_normalized=None	
+
 	def essential_vertices(self):
-		if not self.essential_vertices_:
+		if not self.essential_vertices:
 			self.build_essential_vertices()
-		return self.essential_vertices_
+		return self.essential_vertices
 
 	def edges(self):
-		if not self.edges_:
-			self.build_edges()
-		return self.edges_
+		if not self.has_VE:
+			self.build_VE()
+		return self.edges
 
 	def adjacency(self):
-		if not self.adjacency_:
+		if not self.has_adjacency:
 			self.build_adjacency()
-		return self.adjacency_
+		return self.adjacency
 
 	def build_essential_vertices(self):
+		if not self.has_VE:
+			self.build_VE()
 		pass
 
-	def build_edges(self):
+	def build_VE(self):
 		pass
 
 	def build_adjacency(self):
-		pass
+		if self.adjacency:
+			return
+		if self.vertices:
 
 # def essential_vertices(graph):
 # 	vertices = {}
