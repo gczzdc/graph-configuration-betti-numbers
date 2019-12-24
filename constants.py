ec1b96f0a59e3d089f2be4e302ad72c8a3005b4e
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 11:34:36 -0800

    draft of overall controller

--- constants.py
+++ constants.py
@@ -9,7 +9,17 @@ data_section_title="Data for small graphs"
 img_start_tex='\\documentclass[crop,tikz]{standalone}\n\\begin{document}\n'
 img_end_tex='\\end{document}'
 
+node_radius = '2.5pt'
+
+recompile_images=False
+reconvert_images=False
+run_macaulay=False
+make_files = True
+compile_main=False
+ssh_upload = False
+full_upload = False
 
 
 Betti_row_max_length = 20
 graph_order_of_magnitude=4
+
