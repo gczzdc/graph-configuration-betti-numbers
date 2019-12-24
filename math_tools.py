0dea79fcfb7666d694ce4f49d865e78300faf37e
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 10:59:51 -0800

    draft of backend math tools python

---
+++ math_tools.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def hilb_series_to_coefficient_poly(numerator, denom_power,cutoff=-1):
+	# print (numerator, type(numerator))
+	#hilb series is of the form numerator/(1-T)^denom_power
+	#numerator is formatted as an array starting with LOWEST power.
+	#start by generating the formula for coefficients
+	#output formatted starting with HIGHEST power
+	#should be divided by (denom_power-1) factorial
+	ans = []
+	if denom_power==0:
+		return([])
+	for i in range(len(numerator[:min(cutoff,len(numerator)-1)])+1):
+		this_term=[numerator[i],]
+		for j in range(1,denom_power):
+			this_term=np.polymul(this_term, [1,j-i])
+		ans=np.polyadd(ans,this_term)
+	return [int(n) for n in ans]
