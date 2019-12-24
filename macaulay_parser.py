be6903dd6d9b1afe26ec16b457cd6e396dd1848b
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 12:13:38 -0800

    draft of macaulay parser

---
+++ macaulay_parser.py
@@ -0,0 +1,101 @@
+def parse_macaulay_poly(s):
+	#this looks like eg. T3+6T4-3T5
+	last_power=-1
+	regime = 'coefficient'
+	if s[0]=='-':
+		coefficient_string='-'
+		start_saltus=1
+	else:
+		coefficient_string=''
+		start_saltus=0
+	answer=[]
+	for c in s[start_saltus:]:
+		# print ('c',c,type(c))
+		if c=='T':
+			regime='exponent'
+			exponent_string=''
+			if not coefficient_string:
+				#monic positive nonconstant leading term
+				coefficient=1
+			elif coefficient_string[-1] in '+-':
+				coefficient_string+='1'
+				#monic term
+				coefficient=int(coefficient_string)
+			else:
+				coefficient=int(coefficient_string)
+		elif c in ['+','-']:
+			if regime=='coefficient':
+				#found constant term
+				coefficient=int(coefficient_string)
+				answer.append(coefficient)
+			else:
+				if not exponent_string:
+					exponent=1
+				else:
+					exponent=int(exponent_string)
+				for _ in range(exponent-len(answer)):
+					answer.append(0)
+				answer.append(coefficient)
+			regime='coefficient'
+			coefficient_string=c
+		elif regime=='coefficient':
+			coefficient_string+=c
+		elif regime=='exponent':
+			exponent_string+=c
+	if regime=='coefficient': #constant poly
+		answer.append(int(coefficient_string))
+	else:
+		if not exponent_string:
+			exponent=1
+		else:
+			exponent=int(exponent_string)
+		for _ in range(exponent-len(answer)):
+			answer.append(0)
+		answer.append(coefficient)
+	return(answer)
+
+def process_macaulay_file(mf=macaulay_outfile):
+	f= open(mf,'r')
+	macaulay_data=f.readlines()
+	f.close()
+	answer = {}
+	for j in range(0,len(macaulay_data),5):
+		graph_name=macaulay_data[j].split('Data for graph')[1].strip()
+		if graph_name not in answer:
+			answer[graph_name]= data_class()
+		degree = int(macaulay_data[j+1].split('homological degree')[1].strip())
+		denom_power = int(macaulay_data[j+2].split(':')[1].strip())
+		num_poly = parse_macaulay_poly(macaulay_data[j+3].split(':')[1].strip())
+		valid = len(num_poly) - denom_power - 1
+		answer[graph_name].polys[degree]=(num_poly,denom_power,valid)
+	for G in answer:
+		answer[G].homological_degree=len(answer[G].polys)-1
+		length_test=[]
+		for i in answer[G].polys:
+			length_test.append(int(answer[G].polys[i][2]))
+		maxlen = max(length_test)+1
+		for i in range(answer[G].homological_degree+1):
+			answer[G].Betti_numbers[i]=[]
+			for k in range(maxlen+1):
+				coef_poly= hilb_series_to_coefficient_poly(answer[G].polys[i][0],answer[G].polys[i][1],k)[::-1]
+					#cutting off to get unstable value
+				full_coef_poly = hilb_series_to_coefficient_poly(answer[G].polys[i][0],answer[G].polys[i][1])[::-1]
+					#not cutting off to get stable prediction
+				denom_fact = factorial(answer[G].polys[i][1]-1)
+				this_Betti=0
+				for j,c in enumerate(coef_poly):
+					this_Betti += k**j * c
+				if this_Betti % denom_fact:
+					print (G, i,k, this_Betti, denom_fact, 'error in fraction',answer[G].polys[i])
+					raise(Exception)
+				this_Betti =this_Betti // denom_fact
+				this_Betti_stable=0
+				for j,c in enumerate(full_coef_poly):
+					this_Betti_stable += k**j * c
+				this_Betti_stable = this_Betti_stable / denom_fact
+				##
+				answer[G].Betti_numbers[i].append(int(this_Betti))
+				if this_Betti != this_Betti_stable:
+					answer[G].Betti_number_is_unstable.add((i,k))
+		# generate betti numbers
+	return answer
