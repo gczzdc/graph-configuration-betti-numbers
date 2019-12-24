6379dc8d66f78c18853b451a921bafa236abe2b2
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 12:14:24 -0800

    putting old functions in for later adaptation

--- graph_class.py
+++ graph_class.py
@@ -36,3 +36,28 @@ class Graph():
 
 	def build_adjacency(self):
 		pass
+
+# def essential_vertices(graph):
+# 	vertices = {}
+# 	for edge in graph:
+# 		for v in edge:
+# 			if v not in vertices:
+# 				vertices[v]=0
+# 			vertices[v]+=graph[edge]
+# 	essential_vertices =0
+# 	for v in vertices:
+# 		if vertices[v]>2:
+# 			essential_vertices+=1
+# 	return (essential_vertices)
+
+# def e_pres_to_h_pres(graph):
+# 	h_graph= [sum(graph[1].values()),]
+# 	vertices=list(graph[0].keys())
+# 	h_graph.append([[] for _ in graph[0]])
+# 	counter = 0
+# 	for vert_pair in graph[1]:
+# 		for _ in range(graph[1][vert_pair]):
+# 			for vert in vert_pair:
+# 				h_graph[-1][vertices.index(vert)].append(counter)
+# 			counter+=1
+# 	return h_graph
