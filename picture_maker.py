06fbae4cbf02d7774063dcb95d0fb341402377c5
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 10:54:16 -0800

    rewriting idiomatically

--- picture_maker.py
+++ picture_maker.py
@@ -6,29 +6,21 @@ def image_maker(node_dic, edge_dic, narrow_flag=1, scale=1):
 	out_builder = []
 	out_builder.append("\\begin{tikzpicture}")
 	if scale !=1:
-		out_builder.append("[scale =")
-		out_builder.append(str(scale))
-		out_builder.append("]")
-	out_builder.append("\n")
+		out_builder.append('[scale ={}]'.format(scale))
+	out_builder.append('\n')
 	for node in node_dic:
-		out_builder.append('')
-		out_builder[-1]+='\\fill[black] '
-		out_builder[-1]+=str(node_dic[node])
-		out_builder[-1]+=' circle (' 
-		out_builder[-1]+=node_radius
-		out_builder[-1]+=') node(n'
-		out_builder[-1]+=str(node)
-		out_builder[-1]+='){};\n'
+		out_builder.append(
+			'\\fill[black] {} circle ({}) node(n{}){{}};\n'.format(
+															node_dic[node],
+															node_radius,
+															node))
 	for edge in edge_dic:
 		for j in bend_dic[edge_dic[edge]]:
-			out_builder.append('')
-			out_builder[-1]+= '\\draw (n'
-			out_builder[-1]+= str(edge[0])
-			out_builder[-1]+= '.center) to[bend right='
-			out_builder[-1]+= str(j*narrow_flag)
-			out_builder[-1]+= '] (n'
-			out_builder[-1]+= str(edge[1])
-			out_builder[-1]+='.center);\n'
+			out_builder.append(
+				'\\draw (n{}.center) to[bend right={}] (n{}.center);\n'.format(
+											edge[0],
+											j*narrow_flag),
+											edge[1])			
 	out_builder.append("\\end{tikzpicture}\n")
 	return ''.join(out_builder)
 
