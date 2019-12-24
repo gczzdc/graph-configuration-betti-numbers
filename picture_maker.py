5f0d29e63cdfd5c1a9794e1afdea2361cb4e45bd
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-12-24 10:26:30 -0800

    renaming variables

--- picture_maker.py
+++ picture_maker.py
@@ -1,33 +1,33 @@
 
 def graphmaker(node_dic, edge_dic, narrow_flag=1, scale=1):
-	output = []
-	output.append("\\begin{tikzpicture}")
+	out_builder = []
+	out_builder.append("\\begin{tikzpicture}")
 	if scale !=1:
-		output.append("[scale =")
-		output.append(str(scale))
-		output.append("]")
-	output.append("\n")
+		out_builder.append("[scale =")
+		out_builder.append(str(scale))
+		out_builder.append("]")
+	out_builder.append("\n")
 	for node in node_dic:
-		output.append('')
-		output[-1]+='\\fill[black] '
-		output[-1]+=str(node_dic[node])
-		output[-1]+=' circle (' 
-		output[-1]+=node_radius
-		output[-1]+=') node(n'
-		output[-1]+=str(node)
-		output[-1]+='){};\n'
+		out_builder.append('')
+		out_builder[-1]+='\\fill[black] '
+		out_builder[-1]+=str(node_dic[node])
+		out_builder[-1]+=' circle (' 
+		out_builder[-1]+=node_radius
+		out_builder[-1]+=') node(n'
+		out_builder[-1]+=str(node)
+		out_builder[-1]+='){};\n'
 	for edge in edge_dic:
 		for j in bend_dic[edge_dic[edge]]:
-			output.append('')
-			output[-1]+= '\\draw (n'
-			output[-1]+= str(edge[0])
-			output[-1]+= '.center) to[bend right='
-			output[-1]+= str(j*narrow_flag)
-			output[-1]+= '] (n'
-			output[-1]+= str(edge[1])
-			output[-1]+='.center);\n'
-	output.append("\\end{tikzpicture}\n")
-	return (output)
+			out_builder.append('')
+			out_builder[-1]+= '\\draw (n'
+			out_builder[-1]+= str(edge[0])
+			out_builder[-1]+= '.center) to[bend right='
+			out_builder[-1]+= str(j*narrow_flag)
+			out_builder[-1]+= '] (n'
+			out_builder[-1]+= str(edge[1])
+			out_builder[-1]+='.center);\n'
+	out_builder.append("\\end{tikzpicture}\n")
+	return ''.join(out_builder)
 
 def graph_file(outfile,node_dic,edge_dic,narrow_flag=1,scale=1):
 	data = graphmaker(node_dic,edge_dic,narrow_flag,scale)
