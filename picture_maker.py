334dd9bc29951781d47ee438f1945c4e31b4d7a3
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2020-01-01 22:52:19 -0800

    use weighted edges instead of visible multiple edges

--- picture_maker.py
+++ picture_maker.py
@@ -12,6 +12,9 @@ from constants import (
 	node_radius,
 )
 
+def midpoint(c1, c2):
+	return ((c1[0]+c2[0])/2, (c1[1]+c2[1])/2)
+
 def image_maker(node_dic, edge_dic, narrow=1, scale=1):
 	out_builder = []
 	out_builder.append("\\begin{tikzpicture}")
@@ -36,13 +39,40 @@ def image_maker(node_dic, edge_dic, narrow=1, scale=1):
 	out_builder.append("\\end{tikzpicture}\n")
 	return ''.join(out_builder)	
 
+def weighted_image_maker(node_dic, edge_dic, scale=1):
+	out_builder = []
+	out_builder.append("\\begin{tikzpicture}")
+	if scale !=1:
+		out_builder.append('[scale ={}]'.format(scale))
+	out_builder.append('\n')
+	for node in node_dic:
+		out_builder.append(
+			'\\fill[black] {} circle ({}) node(n{}){{}};\n'.format(
+															node_dic[node],
+															node_radius,
+															node))
+	for edge in edge_dic:
+		out_builder.append('\\draw (n{}.center) to'.format(edge[0]))
+		out_builder.append(' (n{}.center);\n'.format(edge[1]))
+		v0 = node_dic[edge[0]]
+		v1 = node_dic[edge[1]]
+		edge_marker = midpoint(v0,v1)
+		if edge_dic[edge]>1:
+			out_builder.append(
+				'\\draw {} node[inner sep=0, fill=white]{{[{}]}};\n'.format(
+					edge_marker, 
+					edge_dic[edge]
+					))
+			
+	out_builder.append("\\end{tikzpicture}\n")
+	return ''.join(out_builder)	
+
 def compile_image(G, loud_commands):
 	if loud_commands:
 		print ('generating tex file for graph {}'.format(G.name))
 	edge_dic = G.get_edges()
-	data = image_maker(G.image_dic,
+	data = weighted_image_maker(G.image_dic,
 						edge_dic,
-						G.narrow,
 						scale=1)
 	with open(G.filename+'.tex','w') as f:
 		f.write(data)
@@ -52,9 +82,8 @@ def convert_image(G, loud_commands):
 		print ('generating {} file for graph {}'.format(graphics_format, 
 														G.name))
 	edge_dic = G.get_edges()
-	data = image_maker(G.image_dic,
+	data = weighted_image_maker(G.image_dic,
 						edge_dic,
-						G.narrow,
 						scale=2)
 	img_file_base = '{}/{}_img'.format(image_directory, G.filename)
 	with open('{}.tex'.format(img_file_base),'w') as f:
