78a6d996cfe51bd0c4f4255978df6300fa4bb2bf
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2020-01-04 10:22:26 -0800

    formatting output prettification

--- utility.py
+++ utility.py
@@ -5,10 +5,10 @@ from constants import host, remote_directory, loud_commands
 def run(whatever, loud=loud_commands):
 	#this is a dangerous command
 	if loud:
-		print (whatever)
+		print (whatever,end='\n\n')
 	process = subprocess.run(' '.join(whatever), shell=True, capture_output=True)
 	if loud:
-		print(process)
+		print(process, end='\n\n\n')
 
 
 def scp(file_name, loud_commands=loud_commands):
